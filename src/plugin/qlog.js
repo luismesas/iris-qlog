cfc898570bbe052b25b4c0dda256f81a859c52dd
Keko <luismesas@gmail.com>
2013-04-08 18:30:58 +0200

    v0.1

--- src/plugin/qlog.js
+++ src/plugin/qlog.js
@@ -1,4 +1,4 @@
-var qlog = this;
+var qlog = {};
 
 var config = {
 	url : 'http://qlog.io',
@@ -6,18 +6,40 @@ var config = {
 	secretKey : '1234567890ABCDEF1234567890ABCDEF'
 };
 
-var qlog_url = config.url + 'app/' + config.clientId + '/log';
+var qlog_url = '/app/' + config.clientId + '/log';
+
+function _ajax(p_method, p_path, p_params, f_success, f_error) {
+	return iris.ajax({
+		"url": config.url + p_path,
+		"type": p_method,
+		"data": p_params,
+		"cache": false,
+		"dataType": 'json',
+		"async": true,
+		"success": f_success,
+		"error": function (p_request, p_textStatus, p_errorThrown) {
+
+			iris.notify(iris.QLOG_ERROR, {request: p_request, status: p_textStatus, error: p_errorThrown});
+
+			if ( f_error !== undefined ) {
+				f_error();
+			}
+		}
+	});
+}
 
 function _notify(message, tags, f_success, f_error){
 	var params = {
 		msg : message,
 		tags : tags,
-		time : (new Date()).getTime()
+		time : (new Date()).getTime(),
+		secretKey : config.secretKey
 	};
 
-	iris.ajax('PUT', qlog_url, params, f_success, f_error);
+	return _ajax('PUT', qlog_url, params, f_success, f_error);
 }
 
 qlog.notify = _notify;
 
 iris.qlog = qlog;
+iris.QLOG_ERROR = 'QLOG_ERROR';
